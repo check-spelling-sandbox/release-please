b6fe544eadb2a801201c2cf66e7c47a99cb852da
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-19 09:56:56 -0400

    spelling: github
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/factory.ts
+++ src/factory.ts
@@ -132,7 +132,7 @@ export async function buildStrategy(
     changelogSections: options.changelogSections,
   });
   const strategyOptions: BaseStrategyOptions = {
-    skipGitHubRelease: options.skipGithubRelease, // Note the case difference in GitHub
+    skipGitHubRelease: options.skipGitHubRelease, // Note the case difference in GitHub
     skipChangelog: options.skipChangelog,
     ...options,
     targetBranch,
